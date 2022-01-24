ec650e87b76a4d85f9e410f55227dfee7683b8bf
Baris Karalar <karalarbaris@gmail.com>
2022-01-24 10:39:58 +0100

    Initial Commit

---
+++ HIITFit_SwiftUI_Apprentice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HIITFit_SwiftUI_Apprentice
+//
+//  Created by Baris Karalar on 24.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HIITFit_SwiftUI_Apprentice/HIITFit_SwiftUI_ApprenticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFit_SwiftUI_ApprenticeApp.swift
+//  HIITFit_SwiftUI_Apprentice
+//
+//  Created by Baris Karalar on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HIITFit_SwiftUI_ApprenticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
